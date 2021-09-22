87a48d78a8dc4af5af7e5d09494f7cc43081bdb3
Michael Kielpinski <mtsk321@gmail.com>
2021-09-21 19:57:45 -0700

    added content to about page, adjusted typography, and fixed bug with the garmond font

--- src/components/pages/about/aboutComponents/AboutMe.js
+++ src/components/pages/about/aboutComponents/AboutMe.js
@@ -1,11 +1,39 @@
 function aboutMe() {
     return (
-        <section>
-            <h2>About me</h2>
-
-            <h3>my name is Michael, and I am a self taught web devoloper</h3>
-            <p>I have a passion for learning, and enjoy the process of making something new, as well as solving problems along the way</p>
-        </section>
+        <article>
+            <section id="about-intro">
+                <h1>About me</h1>
+                <h2>My name is Michael, and I am a self taught web devoloper</h2>
+                <p>
+                    I'm a bit of a perfectionist in just about everything that i do,
+                    and I am always looking to impove.
+                </p>
+            </section>
+            <div id="skills-div">
+                <section id="hard-skills" class="skills">
+                    <h3>Skills</h3>
+                    <ul>
+                        <li>html</li>
+                        <li>css</li>
+                        <li>scss</li>
+                        <li>javascript</li>
+                        <li>React</li>
+                        <li>Ruby on Rails</li>
+                    </ul>
+                </section>
+                <section id="soft-skills" class="skills">
+                    <h3>Soft skills</h3>
+                    <ul>
+                        <li>Comfortable asking if i don't know</li>
+                        <li>Not only the ablity to lean, but a desire to do so</li>
+                        <li>Happy to work with others, and learn from their experience</li>
+                        <li>Speeking with cutomers</li>
+                        <li>Been in leadership positions</li>
+                        <li>Self motivated</li>
+                    </ul>
+                </section>
+            </div>
+        </article>
     )
 }
 export default aboutMe
